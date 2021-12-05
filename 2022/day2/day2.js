4c18396885684fa63cf764cbe22f9b305f653f10
Mathius <devachievem@gmail.com>
2021-12-04 22:22:40 -0800

    remove console logs

--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -15,30 +15,17 @@ let aim = 0;
 input.forEach((element) => {
     console.log(element[0]);
     if (element[0] === 'forward') {
-        console.log('forward checks out', parseInt(element[1]));
         horizontal += parseInt(element[1])
         vertical += parseInt(aim) * parseInt(element[1])
     }
 
     if (element[0] === 'down') {
-        console.log('down checks out', parseInt(element[1]));
         aim += parseInt(element[1])
     }
 
     if (element[0] === 'up') {
-        console.log('up checks out', parseInt(element[1]));
         aim -= parseInt(element[1])
     }
 });
 
 console.log(horizontal * vertical);
-
-/*
-
-down X increases your aim by X units.
-up X decreases your aim by X units.
-forward X does two things:
-It increases your horizontal position by X units.
-It increases your depth by your aim multiplied by X.
-
-*/

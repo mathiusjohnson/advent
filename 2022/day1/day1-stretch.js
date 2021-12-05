a2835a54c3c8b341e6166d8ad3b2168f3d5dea7f
Mathius <devachievem@gmail.com>
2021-12-04 22:39:37 -0800

    dry day 1

--- 2022/day1/day1-stretch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let input = fs.readFileSync('./input.txt', "utf8");
-
-input = input.split('\n');
-
-// console.log(input);
-
--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -4,7 +4,6 @@ let input = fs.readFileSync('./input.txt', "utf8");
 input = input.split('\n');
 
 let increased = 0;
-let decreased = 0;
 
 for (let i = 0; i < input.length - 1; i++) {
   if (input[i + 1] === undefined) {
@@ -14,15 +13,13 @@ for (let i = 0; i < input.length - 1; i++) {
   if (input[i] - 1 === undefined) {
     continue
   }
-console.log(typeof(input[i]));
+
   let current = parseInt(input[i]) + parseInt(input[i + 1]) + parseInt(input[i + 2])
   let prev = parseInt(input[i- 1]) + parseInt(input[i]) + parseInt(input[i + 1])
-  console.log(current, prev);
+
   if (current > prev) {
     increased++
   }
 }
 
 console.log(increased);
-
-// return increased

8a5978a7ab0abd6bf7722290eebcbf37bdc6ce42
Mathius <devachievem@gmail.com>
2021-12-04 22:23:33 -0800

    dry up code

--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -6,26 +6,22 @@ let input = fs.readFileSync('./input.txt', 'utf8')
     .split('\n')
     .map((arr) => arr.split(' '));
 
-console.log(input);
-
 let horizontal = 0;
 let vertical = 0;
 let aim = 0;
 
 input.forEach((element) => {
-    console.log(element[0]);
-    if (element[0] === 'forward') {
-        horizontal += parseInt(element[1])
-        vertical += parseInt(aim) * parseInt(element[1])
-    }
-
-    if (element[0] === 'down') {
-        aim += parseInt(element[1])
-    }
-
-    if (element[0] === 'up') {
-        aim -= parseInt(element[1])
-    }
+  const firstElement =  parseInt(element[1]); 
+  if (element[0] === 'forward') {
+      horizontal += firstElement
+      vertical += parseInt(aim) * firstElement
+  }
+  if (element[0] === 'down') {
+      aim += firstElement
+  }
+  if (element[0] === 'up') {
+      aim -= firstElement
+  }
 });
 
 console.log(horizontal * vertical);

2b53dceff29933b9bfbd46a13624d9e41ee220eb
Mathius <devachievem@gmail.com>
2021-12-04 22:43:07 -0800

    dry day 1

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -6,14 +6,6 @@ input = input.split('\n');
 let increased = 0;
 
 for (let i = 0; i < input.length - 1; i++) {
-  if (input[i + 1] === undefined) {
-    break
-  }
-
-  if (input[i] - 1 === undefined) {
-    continue
-  }
-
   let current = parseInt(input[i]) + parseInt(input[i + 1]) + parseInt(input[i + 2])
   let prev = parseInt(input[i- 1]) + parseInt(input[i]) + parseInt(input[i + 1])
 
